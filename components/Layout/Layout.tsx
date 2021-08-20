83fd716d83c2d41485b4abb6f69f74be5f34537e
Rozey17 <ferrad.fm@gmail.com>
2021-08-20 16:22:33 +0200

    chore: wip

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -14,7 +14,8 @@ export const Layout: FC<Props> = ({ children, title }: Props) => {
         <ScrollToTop />
         <Navbar />
       </header>
-      <main style={{ minHeight: "calc(100vh - 64px)" }}>{children}</main>
+      {/* <main style={{ minHeight: "calc(100vh - 64px)" }}>{children}</main> */}
+      <main className="min-h-full">{children}</main>
       <footer className="relative bottom-0 w-full p-4 bg-gradient-to-r from-indigo-700 to-blue-500 text-center text-white uppercase">
         Annonce 45
       </footer>
