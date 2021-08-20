37a5fcee4e9250642c899d1e823802d95276fefa
Rozey17 <ferrad.fm@gmail.com>
2021-08-20 16:11:28 +0200

    chore: wip

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -9,13 +9,13 @@ interface Props {
 
 export const Layout: FC<Props> = ({ children, title }: Props) => {
   return (
-    <div className="flex flex-col h-screen">
+    <div>
       <header className="bg-gray-100">
         <ScrollToTop />
         <Navbar />
       </header>
-      <main>{children}</main>
-      <footer className="absolute bottom-0 w-full p-4 bg-gradient-to-r from-indigo-700 to-blue-500 text-center text-white uppercase">
+      <main style={{ minHeight: "calc(100vh - 64px)" }}>{children}</main>
+      <footer className="relative bottom-0 w-full p-4 bg-gradient-to-r from-indigo-700 to-blue-500 text-center text-white uppercase">
         Annonce 45
       </footer>
     </div>
